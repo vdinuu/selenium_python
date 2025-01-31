0dc841f5c6064776e5ce62c210407fd8cd42e616
Dinu V <vdinuu@gmail.com>
2025-01-31 15:25:56 -0500

    Adding config to take screenshot on failure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
+import os.path
+import shutil
+
+import allure
 import pytest
 from selenium import webdriver
 
 driver = None
+
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -9,7 +14,7 @@ def pytest_addoption(parser):
     )
 
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def setup(request):
     global driver
     browser_name = request.config.getoption("browser_name")
@@ -27,5 +32,37 @@ def setup(request):
         driver = webdriver.Edge(options=edge_options)
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     request.cls.driver = driver
-    yield
+    yield driver
     driver.quit()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def delete_allure_results():
+    allure_results_dir = "./allure-results"
+    if os.path.exists(allure_results_dir):
+        shutil.rmtree(allure_results_dir)
+    os.makedirs(allure_results_dir, exist_ok=True)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """
+    Hook to capture screenshot on test failure.
+    """
+    outcome = yield
+    report = outcome.get_result()
+
+    # Check if the test failed
+    if report.when == "call" and report.failed:
+        # Check if the test has a 'driver' fixture (Selenium WebDriver)
+        driver_fixture = item.funcargs.get("setup")
+        if driver_fixture is not None:
+            screenshot_path = "allure-results/screenshot.png"
+            # Take a screenshot
+            driver_fixture.save_screenshot(screenshot_path)
+            # Attach the screenshot to the Allure report
+            allure.attach.file(
+                screenshot_path,
+                name="screenshot",
+                attachment_type=allure.attachment_type.PNG,
+            )
